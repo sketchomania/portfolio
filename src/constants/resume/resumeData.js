34adbf7d21af7e2e7b710fb84c4a1886698824e9
vaibhav <vaibhavvast6529@gmail.com>
2023-05-21 00:57:23 +0530

    added resume

--- src/constants/resume/resumeData.js
+++ src/constants/resume/resumeData.js
@@ -1,11 +1,15 @@
 const resumeData = {
   resume: "Resume",
-  summary: `I'm a developer, my interest and experience lies in building full-stack services,
-  also interested in mobile app development using modern Library and Frameworks.`,
+  summary: `
+    As a developer, I have both interest and experience in building full-stack services. 
+    I’m also passionate about mobile app development and enjoy using modern libraries and frameworks to create innovative solutions.
+  `,
 
-  summarySubText1: `I am working as Mobile Developer Intern, working with the web and mobile team at `,
+  summarySubText1: `I am working as Junior Software Developer, working with the Frontend and Backend team at `,
   summarySubText2: "I am a Computer Science and Engineering Student.",
   achievements: "Achievements",
 };
 
 export default resumeData;
+
+// My expertise in these technologies has enabled me to develop robust and scalable backend systems that meet the needs of my clients.
